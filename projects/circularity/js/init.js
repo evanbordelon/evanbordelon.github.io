6abae95c2d8af32f91b1b70ca636eb9d61137237
evanbordelon <evan.bordelon0903@gmail.com>
2021-02-18 16:13:16 +0000

    adding circularity

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -87,7 +87,7 @@ var init = function (window) {
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
             if ( circle.x < 0) {
 
-                circle.x = 0;
+                circle.x = canvas.width;
 
             }
 
@@ -99,7 +99,7 @@ var init = function (window) {
 
             if ( circle.y < 0){
 
-                circle.y = 0;
+                circle.y = canvas.height;
 
             }
 
